23f130999a9af33305cd37e1220e7442fce34854
beron <bleink@outlook.es>
2020-10-13 11:20:05 +0200

    express const, stores port and views_path

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -1,8 +1,10 @@
 import 'dotenv/config.js'
+import path from 'path'
+
+const envMode = process.env.ENV_MODE || 'development'
 
 export const DB = getDB()
 function getDB() {
-    const envMode = process.env.ENV_MODE || 'development'
     switch (envMode) {
         case 'development':
             return {
@@ -18,6 +20,22 @@ function getDB() {
     }
 }
 
-export const EXPRESS = {
-    PORT: process.env.PORT
+export const EXPRESS = getExpress()
+function getExpress(){
+    switch(envMode){
+        case 'development':
+            const viewsRelativePath = process.env.VIEWS_RELATIVE_PATH.split(',')
+            let viewsPath = path.resolve(process.cwd())
+            viewsRelativePath.forEach(folderName => {
+                viewsPath = path.resolve(viewsPath, folderName)
+            })
+            return {
+                PORT: process.env.PORT,
+                VIEWS_PATH: viewsPath
+            }
+        case 'production':
+            break;
+        default:
+            break;
+    } 
 }
